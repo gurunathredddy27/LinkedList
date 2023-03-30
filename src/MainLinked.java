18effa8cc288fdde60eb02276433375d19f63a5f
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-30 16:32:20 +0530

    added number

--- src/MainLinked.java
+++ src/MainLinked.java
@@ -1,15 +1,49 @@
- class mainLinked{
+    class Node {
+    int data;
+    Node next;
 
-   public class Node{
-        String data;
-        Node next;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
-        Node(String data){
-            this.data = data;
-            this.next = null;
+    class LinkedList {
+    Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
         }
     }
-     public static void main(String[] args) {
 
-     }
- }
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+public class MainLinked {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(56);
+        list.insert(30);
+        list.insert(70);
+        list.display();
+    }
+}
